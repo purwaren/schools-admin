ba7c7cba6550bd278ed31c5aab9190d0721a4116
purwaren <and.thau@gmail.com>
2020-07-04 21:21:04 +0700

    add testdata.sql

---
+++ testdata/testdata.sql
@@ -0,0 +1,6 @@
+INSERT INTO "fee_component" ("id", "name", "amount", "created_by", "created_at", "updated_by", "updated_at") VALUES
+(1,	'SPP Bulanan',	280000,	'100',	'2020-06-29 20:49:05.826244',	'admin',	'2020-06-29 20:59:29.576992'),
+(2,	'Uang Pangkal',	10000000,	'admin',	'2020-06-29 21:00:13.952011',	NULL,	NULL);
+
+INSERT INTO "student" ("id", "nis", "name", "birthdate", "starting_year", "created_by", "created_at", "updated_by", "updated_at") VALUES
+(1,	'001',	'NAFIS PRADANA',	'2016-03-08',	'2020',	'admin',	'2020-07-04 14:09:59.299803',	'admin',	'2020-07-04 14:10:43.156199');
